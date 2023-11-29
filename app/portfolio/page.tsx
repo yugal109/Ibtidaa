3019efd4c1b015dfbe6e88e4196442eb6fd5ee71
aagatsharma <aagats72@gmail.com>
2023-11-29 10:43:00 +0545

    portfolio tabs

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,99 +1,33 @@
-"use client";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import WebContent from "@/components/shared/portfolio/WebContent";
+import AppContent from "@/components/shared/portfolio/AppContent";
+import DigitalContent from "@/components/shared/portfolio/DigitalContent";
 
-import "swiper/css";
-import "swiper/css/grid";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Grid } from "swiper/modules";
-import Image from "next/image";
-
-import bidesh from "@/assets/projectimg/bidesh.png";
-import bishworupi from "@/assets/projectimg/bishworupi.png";
-import bitfly from "@/assets/projectimg/bitfly.png";
-import fastmoney from "@/assets/projectimg/fastmoney.png";
-import rozai from "@/assets/projectimg/rozai.png";
-import tune7 from "@/assets/projectimg/tune7.png";
-
-const projectList = [
-  {
-    img: bidesh,
-    title: "Bidesh",
-  },
-  {
-    img: bishworupi,
-    title: "bishworupi",
-  },
-  {
-    img: bitfly,
-    title: "bitfly",
-  },
-  {
-    img: fastmoney,
-    title: "fastmoney",
-  },
-  {
-    img: rozai,
-    title: "rozai",
-  },
-  {
-    img: tune7,
-    title: "tune7",
-  },
-];
-
-export default function GridCollumn() {
+const page = () => {
   return (
-    <section className=" pt-20 pb-[2rem] min-h-screen ">
-      <div className="lg:mx-auto max-w-7xl mx-[1.5rem]  p-10">
-        <h1 className="text-[3rem] font-bold mb-[2rem] text-center">
-          Projects
-        </h1>
-
-        <Swiper
-          modules={[Grid, Pagination, Navigation]}
-          slidesPerView={3}
-          spaceBetween={20}
-          breakpoints={{
-            0: {
-              slidesPerView: 1,
-            },
-            700: {
-              slidesPerView: 2,
-            },
-            1000: {
-              slidesPerView: 2,
-            },
-          }}
-          navigation
-          grid={{
-            rows: 2,
-          }}
-          className="gridCol "
-        >
-          {projectList.map((p) => {
-            return (
-              <SwiperSlide key={p.title}>
-                <div className="p-5 ">
-                  <div className="flex flex-col items-center">
-                    <div className=" h-[400px] max-md:h-[180px] cursor-pointer overflow-hidden">
-                      <Image
-                        className="scale-1 hover:scale-[1.1] duration-300 "
-                        src={p.img}
-                        height={500}
-                        width={500}
-                        alt={p.title}
-                      />
-                    </div>
-                    <p className="text-white text-3xl pt-4">{p.title}</p>
-                  </div>
-                </div>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-      </div>
-    </section>
+    <div className=" max-w-7xl m-auto pt-20">
+      <h1 className="text-4xl text-center py-10">Our Projects</h1>
+      <Tabs
+        defaultValue="web"
+        className=" flex flex-col items-center max-w-7xl m-auto"
+      >
+        <TabsList>
+          <TabsTrigger value="web">Website</TabsTrigger>
+          <TabsTrigger value="app">Mobile App</TabsTrigger>
+          <TabsTrigger value="digital">Digital Marketing</TabsTrigger>
+        </TabsList>
+        <TabsContent value="web">
+          <WebContent />
+        </TabsContent>
+        <TabsContent value="app">
+          <AppContent />
+        </TabsContent>
+        <TabsContent value="digital">
+          <DigitalContent />
+        </TabsContent>
+      </Tabs>
+    </div>
   );
-}
+};
+
+export default page;
---
+++ components/shared/portfolio/DigitalContent.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import "swiper/css";
+import "swiper/css/grid";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Grid } from "swiper/modules";
+import Image from "next/image";
+
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
+const appList = [
+  {
+    name: "Salaka Farm",
+    description:
+      "Salaka Farm, a pioneering digital marketing channel in Nepal, focuses on elevating agricultural businesses. Specializing in online advertising and social media campaigns, it aims to empower local farmers by enhancing their visibility and connecting them with potential buyers through modern marketing strategies.",
+    path: "/images/salakafarm.png",
+  },
+];
+export default function DigitalContent() {
+  return (
+    <section className=" pt-10 pb-[2rem] min-h-screen ">
+      <div className="lg:mx-auto max-w-7xl ">
+        <Swiper
+          modules={[Grid, Pagination, Navigation]}
+          slidesPerView={3}
+          spaceBetween={10}
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+            },
+            600: {
+              slidesPerView: 2,
+            },
+            1000: {
+              slidesPerView: 3,
+            },
+          }}
+          navigation
+          grid={{
+            rows: 2,
+            fill: "row",
+          }}
+          className="gridCol max-lg:w-screen "
+        >
+          {appList.map((p) => {
+            return (
+              <SwiperSlide key={p.name} className="max-lg:px-5 w-screen ">
+                <Card className="p-5">
+                  <CardHeader className="h-[250px] max-lg:h-[300px] max-md:h-[250px] cursor-pointer overflow-hidden">
+                    <Image
+                      className="scale-1 hover:scale-[1.1] duration-300 object-contain"
+                      src={p.path}
+                      alt=""
+                      height={200}
+                      width={300}
+                    />
+                  </CardHeader>
+                  <CardTitle className="text-center p-3 hover:underline cursor-pointer">
+                    {/* <Link href={p.url} target="_blank"> */}
+                    {p.name}
+                    {/* </Link> */}
+                  </CardTitle>
+                  <CardDescription>{p.description}</CardDescription>
+                </Card>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+    </section>
+  );
+}
